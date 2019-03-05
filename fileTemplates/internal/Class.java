be9aacf94c0b622995b4086e870c1838ad61ebc7
wangjie <wangjie@mxtrip.cn>
2019-03-05 11:06:57 +0800

    IU-2017.2.5 <wangjie@localhost Create baseRefactoring.xml, markdown.xml, ide.general.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, lombok-plugin.xml, myjava.xml, git.xml, other.xml, Default.xml, find.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, projectView.xml, javaeeExternalResources.xml, plugin_ui.xml, colors.scheme.xml, path.macros.xml, databaseDrivers.xml, templates.xml, parameter.hints.xml, Class.java, jdk.table.xml, debugger.renderers.xml, laf.xml, editor.codeinsight.xml, databaseSettings.xml, toStringTemplates.xml, keymap.xml, editor.xml, debugger.xml, Mac OS X copy.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+*
+*@autor ${USER}
+*@date ${YEAR}-${MONTH}-${DAY}
+*/
+public class ${NAME} {
+}
