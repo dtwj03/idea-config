5b84f03d09384ded4571f06733d91594e445bfb5
wangjie <wangjie@mxtrip.cn>
2019-03-05 11:06:56 +0800

    IU-2017.2.5 <wangjie@localhost Overwrite remote https://github.com/dtwj03/idea-config.git to local

--- fileTemplates/internal/Class.java
+++
@@ -1,9 +0,0 @@
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-#parse("File Header.java")
-/**
-*
-*@autor ${USER}
-*@date ${YEAR}-${MONTH}-${DAY}
-*/
-public class ${NAME} {
-}
